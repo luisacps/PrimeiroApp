f00b31d39fbe4ee0348dd33a0fa33ad4b4a0cdb7
Luisa <luisacpsantos@gmail.com>
2024-03-22 09:09:44 -0300

    Codigo comentado

--- app/src/main/java/santos/pimentel/primeiroapp/MainActivity.java
+++ app/src/main/java/santos/pimentel/primeiroapp/MainActivity.java
@@ -14,15 +14,24 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // leitura do arquivo de layout e construcao de interface
         setContentView(R.layout.activity_main);
+        // obtem o botao de enviar presente na interface
         Button btnEnviar = findViewById(R.id.btnEnviar);
+        // definicao do que acontece ao clicar no botao
         btnEnviar.setOnClickListener(new View.OnClickListener() {
             @Override
+            // metodo recrutado toda vez que ha um click no botao
             public void onClick(View v) {
+                // obtem a caixa de texto presente na interface
                 EditText etDigiteAqui = findViewById(R.id.etDigiteAqui);
+                // captura o texto digitado na caixa de texto
                 String textoDigitado = etDigiteAqui.getText().toString();
+                // cria uma intencao de navegacao da MainActivity para a NextActivity
                 Intent i = new Intent(MainActivity.this, NextActivity.class);
+                // coloca o texto ja coletado dentro da intencao para que ele seja enviado para a NextActivity
                 i.putExtra("texto", textoDigitado);
+                // execucao da intencao e navegacao para a proxima tela
                 startActivity(i);
             }
         });
--- app/src/main/java/santos/pimentel/primeiroapp/NextActivity.java
+++ app/src/main/java/santos/pimentel/primeiroapp/NextActivity.java
@@ -12,9 +12,13 @@ public class NextActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next);
+        // obtem o intent que navegou ate essa tela
         Intent i = getIntent();
+        // obtem o texto que foi colocado na intent
         String texto = i.getStringExtra("texto");
+        // obtem a caixa de visualizacao de texto presente na interface
         TextView tv = findViewById(R.id.tvTexto);
+        // coloca o texto coletado da intent na caixa de visualizacao de texto
         tv.setText(texto);
     }
 }
